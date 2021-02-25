724a71e30bbf6f954ffc2e4aede0684b8271c4b2
97rocko <97rocko@gmail.com>
2021-02-24 22:57:16 -0600

    added two more API options

--- index.js
+++ index.js
@@ -2,22 +2,35 @@ var express = require("express");
 var axios = require("axios");
 var cors = require('cors');
 var app = express();
-let port = process.env.PORT || 3100;
+let port = process.env.PORT || 3000;
 
 app.use(cors())
 
 app.get("/", (req, res) => {
-    res.send("API is functioning");
+    res.send("API is online! Contact Rocko#8625 on Discord with questions.");
 });
 
-app.get("/users", (req, res, next) => {
-    var url = 'https://accounts.rec.net/account?username=' + req.query.user;
-    var result = '';
-    axios.get(url)
-        .then(response => {
-            res.json(response.data);
-            console.log(response.data);
-        });
+// Takes a player's username or ID and returns back data on the user
+app.get("/account/", (req, res) => {
+    if (req.query.u) { // ?u={username}
+        var url = 'https://accounts.rec.net/account?username=' + req.query.u;
+        axios.get(url)
+            .then(response => {
+                res.json(response.data);
+            });
+    } else if (req.query.id) { // ?id={playerId}
+        var url = 'https://accounts.rec.net/account/' + req.query.id;
+        axios.get(url)
+            .then(response => {
+                res.json(response.data);
+            });
+    } else if (req.query.bio) { // ?bio={playerId}
+        var url = 'https://accounts.rec.net/account/' + req.query.bio + '/bio';
+        axios.get(url)
+            .then(response => {
+                res.json(response.data);
+            });
+    }
 });
 
 app.listen(port, () => {
