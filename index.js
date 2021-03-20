f38a397cdb844a16aa4887f6cafd3b40e9566586
97rocko <97rocko@gmail.com>
2021-03-20 02:13:25 -0500

    added new global end point

--- index.js
+++ index.js
@@ -37,13 +37,59 @@ app.get("/account/", (req, res) => {
     }
 });
 
+app.get("/images/global/", async (req, res) => {
+    // Possible Parameters
+    // - Date
+    // - Take
+    
+    const END_POINT_ADDRESS = '/images/global : '
+    if (req.query.take || req.query.date) {
+        var dtToday = moment().format();
+        var iReturnAmount = 2000;
+        if (req.query.take) {
+            var iTake = parseInt(req.query.take)
+            if (iTake > 0) {iReturnAmount = iTake}
+        }
+
+        var date = 0;
+        if (req.query.date) {
+            date = moment(new Date(req.query.date)).format();
+        }
+
+        if (date < dtToday) { // This date isnt working correctly.. but the take is working fine
+            dtToday = date
+        }
+
+        var szUrl = 'https://api.rec.net/api/images/v3/feed/global?skip=0&take=' + iReturnAmount + '&since=' + dtToday
+        var oGlobalData = await recnet.getData(szUrl);
+        res.json(oGlobalData);
+    
+    } else {
+        res.send(END_POINT_ADDRESS & "Supplied parameter is not permitted.");
+        return;
+    }
+});
+
 app.get("/images/", async (req, res) => {
+
+    // List of possible query parameters (This should probably become request JSON eventually)
+    // 
+    // - Type [0-3]
+    // - Filter [String]
+    // - Sort [String]
+    // - uid [integer]
+    // - u [username]
+
+
     // console.log(req.query);
     const END_POINT_ADDRESS = '/images/ : '
     var userInfoObject;
     var accountId;
 
-    if (req.query.u && req.query.uid) res.sendStatus(END_POINT_ADDRESS & "Only username OR user ID can be supplied.");
+    if (req.query.u && req.query.uid) {
+        res.send(END_POINT_ADDRESS & "Only username OR user ID can be supplied.");
+        return;
+    }
 
     if (req.query.u) { // ?u={username}
         // TODO ADD VALIDATION TO MAKE SURE THIS HAS A MAX LENGTH
@@ -66,7 +112,7 @@ app.get("/images/", async (req, res) => {
         }
     }
 
-    if (accountId >= 1) {
+    if (accountId >= 1) { // types 1 and 2 should be evaluated in here
         var url = 'https://api.rec.net/api/images/v4/player/' + accountId; // Default if type is not provided
         if (req.query.type) {
             var type = parseInt(req.query.type);
@@ -90,6 +136,7 @@ app.get("/images/", async (req, res) => {
         }
 
 
+        // type 0 should be evaluated above.  If type 0 then set accountId = -1
 
         // var dtToday = moment().format();
         // var urlUserPhotos = 'https://api.rec.net/api/images/v4/player/' + userId + '?skip=0&take=100000';
